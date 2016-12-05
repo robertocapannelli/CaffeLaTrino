de4c65594a3d67c1fb2d299fad3f28b9306987db
Roberto Capannelli <roberto.capannelli@gmail.com>
2016-12-05 16:52:36 +0100

    intent that carries kind of of coffee to the next activity from radio button selected

--- app/src/main/java/com/walkap/caffelatrino/ChooseCoffeeActivity.java
+++ app/src/main/java/com/walkap/caffelatrino/ChooseCoffeeActivity.java
@@ -1,13 +1,32 @@
 package com.walkap.caffelatrino;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class ChooseCoffeeActivity extends AppCompatActivity {
+
+    public RadioGroup radioCoffeeGroup;
+    public RadioButton radioCoffeeButton;
+    public final static String EXTRA_MESSAGE = "com.example.caffelatrino.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_coffee);
+    }
+
+    /*Called when button "Go" is pressed*/
+    public void sendMessage(View view){
+        Intent intent = new Intent(this, SugarSlider.class);
+        radioCoffeeGroup = (RadioGroup) findViewById(R.id.radioCoffeeGroup);
+        int selectedId = radioCoffeeGroup.getCheckedRadioButtonId();
+        radioCoffeeButton = (RadioButton) findViewById(selectedId);
+        String coffee = radioCoffeeButton.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, coffee);
+        startActivity(intent);
     }
 }
