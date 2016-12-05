5e5e5bd64d02d404ca01de43a21d2ae13dcfd26d
Roberto Capannelli <roberto.capannelli@gmail.com>
2016-12-05 18:52:41 +0100

    added message from intent of previous activity and set progress for the seekbar

--- app/src/main/java/com/walkap/caffelatrino/SugarSlider.java
+++ app/src/main/java/com/walkap/caffelatrino/SugarSlider.java
@@ -1,12 +1,17 @@
 package com.walkap.caffelatrino;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.ViewGroup;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
 public class SugarSlider extends AppCompatActivity {
+
+
+    public SeekBar slider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +25,10 @@ public class SugarSlider extends AppCompatActivity {
 
         ViewGroup layout = (ViewGroup) findViewById(R.id.activity_sugar_slider);
         layout.addView(textView);
+
+        slider = (SeekBar) findViewById(R.id.slider);
+        slider.setMax(5);
+        slider.setProgress(1);
 
     }
 
