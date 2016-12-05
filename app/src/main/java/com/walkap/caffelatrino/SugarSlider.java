4b4d40951fcdea9838ecf60fc76d9ce70d1ad21a
Roberto Capannelli <roberto.capannelli@gmail.com>
2016-12-05 23:41:28 +0100

    added classes that permit to get the value of the seekbar

--- app/src/main/java/com/walkap/caffelatrino/SugarSlider.java
+++ app/src/main/java/com/walkap/caffelatrino/SugarSlider.java
@@ -1,17 +1,20 @@
 package com.walkap.caffelatrino;
 
-import android.content.Context;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.ViewGroup;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
 import android.widget.TextView;
 
-public class SugarSlider extends AppCompatActivity {
+//import static com.walkap.caffelatrino.ChooseCoffeeActivity.EXTRA_COFFEE;
 
+public class SugarSlider extends AppCompatActivity implements OnSeekBarChangeListener {
 
     public SeekBar slider;
+    public int value;
+    //public final static String EXTRA_SUGAR = "com.example.caffelatrino.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,17 +22,33 @@ public class SugarSlider extends AppCompatActivity {
         setContentView(R.layout.activity_sugar_slider);
 
         Intent intent = getIntent();
-        String message = intent.getStringExtra(ChooseCoffeeActivity.EXTRA_MESSAGE);
-        TextView textView = new TextView(this);
-        textView.setText("Coffee: " + message);
-
-        ViewGroup layout = (ViewGroup) findViewById(R.id.activity_sugar_slider);
-        layout.addView(textView);
+        String coffee = intent.getStringExtra("extra_coffee");
+        TextView tvCoffee = (TextView) findViewById(R.id.tvCoffee);
+        tvCoffee.setText("Coffee: " + coffee);
 
         slider = (SeekBar) findViewById(R.id.slider);
         slider.setMax(5);
         slider.setProgress(1);
-
+        slider.setOnSeekBarChangeListener(this);
     }
+
+    @Override
+    public void onProgressChanged(SeekBar slider, int progress, boolean fromUser) {
+        value = progress;
+    }
+    @Override
+    public void onStartTrackingTouch(SeekBar slider) {
+    }
+    @Override
+    public void onStopTrackingTouch(SeekBar slider) {
+    }
+
+    public void sendMessage(View view){
+        Intent intent = new Intent(this, AdditionActivity.class);
+        String seekValue = "" + value;
+        intent.putExtra("extra_sugar", seekValue);
+        startActivity(intent);
+    }
+
 
 }
